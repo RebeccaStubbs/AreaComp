9edbfa34b0ef3ce4e44456dc58952b0744d178e6
Rebecca Stubbs <stubbsrw@Drs-MacBook-Air.local>
2018-03-06 10:38:40 -0700

    add sandbox script

---
+++ sandbox.R
@@ -0,0 +1,9 @@
+######
+# Author: Rebecca Stubbs
+# Date: March 6, 2017 
+# Purpose: Test out strategies and functions for area composition!
+
+# Load libraries
+library(Mapsuite)
+library(sf) #Simple features is the future!
+
